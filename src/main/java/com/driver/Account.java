d88455789f4c40ea24c3f742a7bb671b650b2070
saymedgm <deepak.cse.dtu@gmail.com>
2024-02-24 00:10:00 +0530

    6

--- src/main/java/com/driver/Account.java
+++ src/main/java/com/driver/Account.java
@@ -7,6 +7,7 @@ public class Account {
     //dasdas
     public int balance = 1000;
 //    https://github.com/acciojob/whatsapp-money-transfer-saymedgm.git
+    // changes
     public Account(String name) {
         this.accountName = name;
     }
